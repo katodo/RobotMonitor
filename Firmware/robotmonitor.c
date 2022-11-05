a28ec3f6e13c97ee2f2a8ed741d7185783de3412
katodo <mauro.soligo@katodo.com>
2022-11-05 18:53:42 +0100

    Initiale test on RP2040

---
+++ Firmware/robotmonitor.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "pico/binary_info.h"
+#include "ws2812.pio.h"
+
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
+#include <sensor_msgs/msg/battery_state.h>
+#include <rmw_microros/rmw_microros.h>
+#include "pico_uart_transports.h"
+
+const uint LED_PIN = 25;
+
+//#define LED_PIN_WS 23
+//#define LED_LENGTH 1
+const int PIN_TX = 23;
+
+static inline void put_pixel(uint32_t pixel_grb) {
+  pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
+  return ((uint32_t)(r) << 8) |
+         ((uint32_t)(g) << 16) |
+         (uint32_t)(b);
+}
+
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+
+rcl_publisher_t publisher_batt;
+sensor_msgs__msg__BatteryState msgBattery;
+rosidl_runtime_c__float__Sequence battVoltage;
+const uint NUMBEROFFCELL = 6;
+
+
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+{
+    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+    ret += rcl_publish(&publisher_batt, &msgBattery, NULL);
+    msg.data++;
+    msgBattery.capacity++;
+    msgBattery.cell_voltage.data[0] += 0.05;
+}
+
+int main()
+{
+
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    char str[12];
+
+    ws2812_program_init(pio, sm, offset, PIN_TX, 800000, true);
+    put_pixel(urgb_u32(0xff, 0, 0));  // Red
+
+    rmw_uros_set_custom_transport(
+		true,
+		NULL,
+		pico_serial_transport_open,
+		pico_serial_transport_close,
+		pico_serial_transport_write,
+		pico_serial_transport_read
+	);
+
+
+
+    rcl_timer_t timer;
+    rcl_node_t node;
+    rcl_allocator_t allocator;
+    rclc_support_t support;
+    rclc_executor_t executor;
+
+    allocator = rcl_get_default_allocator();
+
+    // Wait for agent successful ping for 2 minutes.
+    const int timeout_ms = 1000; 
+    const uint8_t attempts = 120;
+
+    rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
+
+    if (ret != RCL_RET_OK)
+    {
+        // Unreachable agent, exiting program.
+        return ret;
+    }
+
+    rclc_support_init(&support, 0, NULL, &allocator);
+
+    rclc_node_init_default(&node, "pico_node", "", &support);
+
+    rclc_publisher_init_default(
+        &publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "pico_publisher");
+        
+    msgBattery.power_supply_status = sensor_msgs__msg__BatteryState__POWER_SUPPLY_STATUS_UNKNOWN;
+    msgBattery.power_supply_health = sensor_msgs__msg__BatteryState__POWER_SUPPLY_HEALTH_UNKNOWN;
+	msgBattery.power_supply_technology = sensor_msgs__msg__BatteryState__POWER_SUPPLY_TECHNOLOGY_LION;
+    msgBattery.charge = 2;
+    msgBattery.current = 10;
+    battVoltage.size = NUMBEROFFCELL;
+    msgBattery.cell_voltage.capacity = NUMBEROFFCELL;
+    msgBattery.cell_voltage.size = NUMBEROFFCELL;
+    msgBattery.cell_voltage.data = ( float*) malloc( msgBattery.cell_voltage.capacity * sizeof(float));
+
+    msgBattery.cell_voltage.data[0] = 0;
+    msgBattery.cell_voltage.data[1] = 1;
+    msgBattery.cell_voltage.data[3] = 3;
+
+
+    rclc_publisher_init_default( 
+        &publisher_batt, 
+        &node, 
+        ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, BatteryState), 
+        "pBatt");
+
+
+    rclc_timer_init_default(
+        &timer,
+        &support,
+        RCL_MS_TO_NS(1000),
+        timer_callback);
+
+    rclc_executor_init(&executor, &support.context, 1, &allocator);
+    rclc_executor_add_timer(&executor, &timer);
+
+    gpio_put(LED_PIN, 1);
+    // OPTION B: comment sleep_ms(500) and view the pixels cascading colors
+    put_pixel(urgb_u32(0xff, 0, 0));  // Red
+    sleep_ms(500);
+    put_pixel(urgb_u32(0, 0xff, 0));  // Green
+    sleep_ms(500);
+    put_pixel(urgb_u32(0, 0, 0xff));  // Blue
+    sleep_ms(500);
+    put_pixel(urgb_u32(0xff, 0xff, 0));  // Purple
+    sleep_ms(500);
+    put_pixel(urgb_u32(0, 0xff, 0xff));  // Cyan
+    sleep_ms(500);
+    put_pixel(urgb_u32(0xff, 0xff, 0xff));  // White
+    sleep_ms(500);
+    put_pixel(urgb_u32(0, 0, 0));  // Black or off
+    sleep_ms(500);
+
+    msg.data = 0;
+
+    while (true)
+    {
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+    }
+    return 0;
+}
